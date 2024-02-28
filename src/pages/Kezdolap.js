3673a5ed5e7951239642fdc62ea9c9a04eb83d60
s.v.gyongyi <s.v.gyongyi@gmail.com>
2024-02-28 20:12:13 +0100

    regisztráció-breeze

---
+++ src/pages/Kezdolap.js
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import useAuthContext from "../contexts/AuthContext";
+
+export default function Kezdolap() {
+    const { user, getUser } = useAuthContext();
+    useEffect(() => {
+        console.log(user)
+        if (!user) {
+            getUser();
+        }
+    });
+    return (
+        <div>
+            <h1>Kezdőlap</h1>
+            <p>Bejelentkezett felhasználó: {user?.name}</p>
+        </div>
+    );
+}
